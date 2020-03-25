7dd1ce526146a0a2baa75b187dcce2628931abc4
code4lala <fenglala@outlook.com>
2020-03-25 17:58:39 +0800

    login done

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,13 +2,37 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import Global from "@/store/global";
+import router from "@/router";
 
 Vue.use(Vuex);
+
+const cl = console.log
+const ce = console.error
+
+// 以下是客户端服务端公共部分 {{{
+
+enum MSG_BACK_TYPE {
+  LOGIN_SUCC,
+  LOGIN_FAIL,
+}
 
 enum MSG_TYPE {
   LOGIN,
   CHAT,
 }
+
+// 以上是客户端服务端公共部分 }}}
+
+const MSG_HANDLER: any = []
+MSG_HANDLER[MSG_BACK_TYPE.LOGIN_SUCC] = function (data: string) {
+  cl('登录成功啦')
+  cl('服务端返回的消息为: ' + data)
+  router.push('/')
+}
+MSG_HANDLER[MSG_BACK_TYPE.LOGIN_FAIL] = function () {
+  // TODO
+}
+
 
 function fnBuildMsg(type: MSG_TYPE, content: string): string {
   return JSON.stringify({
@@ -25,7 +49,7 @@ export default new Vuex.Store<any>({
   },
   mutations: {
     fnInitConnection() {
-      console.log('init websocket...')
+      cl('init websocket...')
 
       // 开启连接
       Global.connection = new WebSocket('ws://127.0.0.1:10086')
@@ -34,52 +58,56 @@ export default new Vuex.Store<any>({
 
       Global.connection.onopen = function (event: any) {
         // 连接已就绪 TODO
-        console.log('成功连接到服务器')
-        console.log(event.target.url)
+        cl('成功连接到服务器')
+        cl(event.target.url)
       }
 
       Global.connection.onerror = function (event: Event) {
         // 发送/接收数据时失败 TODO
-        console.log('发送或接收数据时发生错误如下:')
-        console.log(event)
+        ce('发送或接收数据时发生错误如下:')
+        ce(event)
       }
 
       // 处理服务器发来的消息
       Global.connection.onmessage = function (messageEvent: MessageEvent) {
         // 处理消息 TODO
-        let json = null
+        let msgObj = null
         try {
-          json = JSON.parse(messageEvent.data)
+          msgObj = JSON.parse(messageEvent.data)
         } catch (e) {
-          console.error(e)
-          console.log('返回数据不是JSON: '
-            + messageEvent.data)
+          ce(e)
+          ce('服务端返回数据不是JSON: ' + messageEvent.data)
           return
         }
+
+        MSG_HANDLER[msgObj.type](msgObj.data)
+
+        // TODO 写到这里啦....!!!!!!
 
         // 填充消息
         function addMessage(author: string, message: string, dt: string) {
           // TODO 填充消息
-          console.log(author + message + dt)
+          cl(author + message + dt)
         }
 
-        // 处理接收到的消息
-        if (json.type === 'history') {
 
-          for (let i = 0; i < json.data.length; i++) {
-            addMessage(json.data[i].author, json.data[i].text,
-              new Date(json.data[i].time).toLocaleString())
+        // 处理接收到的消息
+        if (msgObj.type === 'history') {
+
+          for (let i = 0; i < msgObj.data.length; i++) {
+            addMessage(msgObj.data[i].author, msgObj.data[i].text,
+              new Date(msgObj.data[i].time).toLocaleString())
           }
-        } else if (json.type === 'message') {
+        } else if (msgObj.type === 'message') {
           // TODO 设置不可以输入
-          addMessage(json.data.author, json.data.text,
-            new Date(json.data.time).toLocaleString())
-        } else if (json.type === 'name_set') {
+          addMessage(msgObj.data.author, msgObj.data.text,
+            new Date(msgObj.data.time).toLocaleString())
+        } else if (msgObj.type === 'name_set') {
           // TODO 设置可以输入
-          addMessage(json.data.author, '用户名为' + json.data.text,
-            new Date(json.data.time).toLocaleString())
+          addMessage(msgObj.data.author, '用户名为' + msgObj.data.text,
+            new Date(msgObj.data.time).toLocaleString())
         } else {
-          console.log('接收到的消息格式不正确')
+          cl('接收到的消息格式不正确')
         }
       }
     },
@@ -87,15 +115,15 @@ export default new Vuex.Store<any>({
     fnSendByConnection: function (msgContent: string) {
       Global.connection.send(msgContent)
       // TODO 发送消息
-      console.log('消息: `' + msgContent + '`已发送')
+      cl('消息: `' + msgContent + '`已发送')
 
     },
 
     fnLoginByConnection: function (state: any, userName: string) {
       // TODO 登录设置用户名
-      console.log('登录用户名为' + userName)
+      cl('登录用户名为' + userName)
       Global.connection.send(fnBuildMsg(MSG_TYPE.LOGIN, userName))
-      console.log('正在登录')
+      cl('正在登录')
     }
   },
   actions: {},
--- server/index.ts
+++ server/index.ts
@@ -1,49 +1,73 @@
+import {IncomingMessage, ServerResponse} from "http";
+
 process.title = 'how_are_you_server'
 const webSocketServerPort = 10086
 const webSocketServer = require('websocket').server
 const http = require('http')
+
+let chatHistory: any = []
+let clients: any = []
+
+// clt = console log time       cl = console log
+// cet = console error time     ce = console error
+const clt = function (arg: any) {
+  console.log((new Date()) + ' ' + arg)
+}
+const cl = console.log
+const cet = function (arg: any) {
+  console.error((new Date()) + ' ' + arg)
+}
+const cer = console.error
+
+// 以下是客户端服务端公共部分 {{{
+
+enum MSG_BACK_TYPE {
+  LOGIN_SUCC,
+  LOGIN_FAIL,
+}
 
 enum MSG_TYPE {
   LOGIN,
   CHAT,
 }
 
-let chatHistory = []
-let clients = []
-const clog = function (arg) {
-  console.log((new Date()) + ' ' + arg)
-}
+// 以上是客户端服务端公共部分 }}}
 
-function htmlEntities(str) {
-  return String(str)
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
+let MSG_HANDLER: any = []
+MSG_HANDLER[MSG_TYPE.LOGIN] = function (conn: any, data: string) {
+  clt('登录消息处理器')
+  // TODO 判断用户名密码啥的对不对
+  conn.sendUTF(JSON.stringify({
+    type: MSG_BACK_TYPE.LOGIN_SUCC,
+    data: '登录成功'
+  }))
+}
+MSG_HANDLER[MSG_TYPE.CHAT] = function (conn: any, data: string) {
+  clt('聊天内容消息处理器')
+  cl(data)
 }
 
 // HTTP server
-const server = http.createServer(function (request, response) {
-  clog('创建http server' + request + response)
+const server = http.createServer(function (request: IncomingMessage, response: ServerResponse) {
+  clt('创建http server' + request + response)
 })
 server.listen(webSocketServerPort, function () {
-  clog('正在监听端口' + webSocketServerPort)
+  clt('正在监听端口' + webSocketServerPort)
 })
 // WebSocket server
 const wsServer = new webSocketServer({
   httpServer: server
 })
 
-// 连接回调函数
-wsServer.on('request', function (request) {
-  clog('源 ' + request.origin + ' 已连接')
+function wsServerOnRequest(request: any) {
+  clt('源 ' + request.origin + ' 已连接')
 
   // 接受连接
   const connection = request.accept(null, request.origin)
   const index = clients.push(connection) - 1
   let userName = ''
 
-  clog('Connection accepted')
+  clt('Connection accepted')
 
   // 返回历史消息
   if (chatHistory.length > 0) {
@@ -53,43 +77,50 @@ wsServer.on('request', function (request) {
   }
 
   // 用户发送消息
-  connection.on('message', function (message) {
-    if (message.type === 'utf8') {
-      if (userName === '') {
-        // 记住用户名
-        userName = htmlEntities(message.utf8Data)
-        connection.sendUTF(JSON.stringify({
-          type: 'name_set',
-          data: userName
-        }))
+  connection.on('message', function (message: any) {
+    if (message.type !== 'utf8') return
+    if (userName === '') {
+      // 记住用户名
+      let msgObj
+      try {
+        msgObj = JSON.parse(message.utf8Data)
+      } catch (e) {
+        cer(e)
+        cet('解析客户端发来的消息时候出错啦')
+      }
+      cl(msgObj)
+      MSG_HANDLER[msgObj.type](connection, msgObj.data)
+      connection.sendUTF(JSON.stringify({
+        type: 'name_set',
+        data: userName
+      }))
 
-        clog('User is known as: ' + userName)
-      } else {
-        // 登录并广播
-        clog('Received message from '
-          + userName + ': ' + message.utf8Data)
+      clt('User is known as: ' + userName)
+    } else {
+      // 登录并广播
+      clt('Received message from '
+        + userName + ': ' + message.utf8Data)
 
-        // 保存所有已发送消息
-        const msgObj = {
-          time: (new Date()).getTime(),
-          text: htmlEntities(message.utf8Data),
-          author: userName,
-        }
-        chatHistory.push(msgObj)
+      // 保存所有已发送消息
+      const msgObj = {
+        time: (new Date()).getTime(),
+        text: message.utf8Data,
+        author: userName,
+      }
+      chatHistory.push(msgObj)
 
-        // 向所有已登录客户端广播消息
-        const json = JSON.stringify({type: 'message', data: msgObj})
-        for (let i = 0; i < clients.length; i++) {
-          clients[i].sendUTF(json)
-        }
+      // 向所有已登录客户端广播消息
+      const json = JSON.stringify({type: 'message', data: msgObj})
+      for (let i = 0; i < clients.length; i++) {
+        clients[i].sendUTF(json)
       }
     }
   })
 
   // 用户断开连接
-  connection.on('close', function (connection) {
+  connection.on('close', function (connection: any) {
     if (userName !== '') {
-      clog('Peer '
+      clt('Peer '
         + connection.remoteAddress + ' disconnected')
 
       // 从已连接列表中移除该客户端
@@ -97,16 +128,18 @@ wsServer.on('request', function (request) {
     }
   })
 
-  connection.on('error', function (error) {
+  connection.on('error', function (error: any) {
     if (error.code === 'ECONNRESET') {
-      clog('用户离线')
+      clt('用户离线')
       // TODO
     } else {
-      clog('连接出错: ')
-      clog(error)
+      clt('连接出错: ')
+      clt(error)
     }
   })
-})
+}
 
+// 连接回调函数
+wsServer.on('request', wsServerOnRequest)
 
-clog('last line')
+clt('last line')
