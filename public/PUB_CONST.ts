456583d310c1c32de20ddd04400195fc15ac9a3b
code4lala <fenglala@outlook.com>
2020-05-05 19:49:24 +0800

    chat.code4lala.vip

--- public/PUB_CONST.ts
+++ public/PUB_CONST.ts
@@ -1,24 +1,7 @@
-import {clt} from "../server/Util";
-
-function fnGetIp() {
-  const os = require('os')
-  const interfaces = os.networkInterfaces()
-  let IPv4 = '127.0.0.1'
-  for (const key in interfaces) {
-    interfaces[key].forEach(function (details: any) {
-      if (details.family === 'IPv4' && key === '以太网') {
-        IPv4 = details.address.toString()
-        clt(details.address + ' ' + key)
-      }
-    })
-  }
-  return IPv4
-}
-
 export default class PUB_CONST {
   static readonly SERVER_PORT = 10086
   static readonly FILE_SERVER_PORT = 10010
-  static readonly HOW_ARE_YOU_HOST = fnGetIp()
+  static readonly HOW_ARE_YOU_HOST = 'chat.code4lala.vip'
   static readonly HOW_ARE_YOU_URL =
     'http://' + PUB_CONST.HOW_ARE_YOU_HOST
   static readonly WEBSOCKET_SERVER_URL =
@@ -27,7 +10,7 @@ export default class PUB_CONST {
   static readonly API_FILE = '/api/file'
   static readonly DOWNLOAD_FILE = '/download/file'
   // see <project_root>/client/package.json scripts serve
-  static readonly APP_FRONT_END_PORT = 8888
+  static readonly APP_FRONT_END_PORT = 443
   static readonly UPLOAD_FILE_URL =
     PUB_CONST.HOW_ARE_YOU_URL + ':' +
     PUB_CONST.FILE_SERVER_PORT +
