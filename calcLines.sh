aa665dc5f24253cf17509b8de51127d59c93be89
code4lala <fenglala@outlook.com>
2020-05-09 09:25:52 +0800

    calc lines sum

--- calcLines.sh
+++ calcLines.sh
@@ -1,3 +1,10 @@
-find -name "*.ts" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'
-find -name "*.vue" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'
-find -name "*.js" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'
+cat $0
+ts_len=`find -name "*.ts" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'`
+vue_len=`find -name "*.vue" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'`
+js_len=`find -name "*.js" -exec wc -l "{}" \; | awk '{s+=$1}END{print s}'`
+echo -e "ts行数\t$ts_len"
+echo -e "vue行数\t$vue_len"
+echo -e "js行数\t$js_len"
+sum_len=`expr $ts_len + $vue_len + $js_len`
+echo -e "总行数\t$sum_len"
+
